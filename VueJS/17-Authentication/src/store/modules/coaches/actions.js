52cf46a4639a28101df21531bc3f51d2da3c8daf
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-11-19 15:43:34 +0700

    17-Authentication

--- VueJS/17-Authentication/src/store/modules/coaches/actions.js
+++ VueJS/17-Authentication/src/store/modules/coaches/actions.js
@@ -10,7 +10,7 @@ export default {
     };
 
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/coaches/${userId}.json`,
+      `https://learn-vue-http-demo-default-rtdb.firebaseio.com/coaches/${userId}.json`,
       {
         method: 'PUT',
         body: JSON.stringify(coachData)
@@ -34,7 +34,7 @@ export default {
     }
 
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/coaches.json`
+      `https://learn-vue-http-demo-default-rtdb.firebaseio.com/coaches.json`
     );
     const responseData = await response.json();
 
